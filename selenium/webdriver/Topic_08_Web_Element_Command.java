6da48927f661625bbf42627cc9fd0128250b14e6
Phuong anh <phuonganhttcb@gmail.com>
2023-07-03 18:37:40 +0700

    bai 21

--- selenium/webdriver/Topic_08_Web_Element_Command.java
+++ selenium/webdriver/Topic_08_Web_Element_Command.java
@@ -1,13 +1,64 @@
 package webdriver;
 
-public class Topic_08_Web_Element_Command {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String projectPath = System.getProperty("user.dir");
-		System.out.println(projectPath);
-		
-		String osName = System.getProperty("os.name");
-		System.out.println(osName);
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_08_Web_Element_Command {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	JavascriptExecutor jsExecutor;
+	Actions action;
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac")) { // Mac
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		} else { // Windows
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+
+		driver = new FirefoxDriver();
+		jsExecutor = (JavascriptExecutor) driver;
+		action = new Actions(driver);
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 	}
+
+	@Test
+	public void TC_01_WebElement() {
+		// Element: textbox/ textarea/ dropdown/ checkbox/ radio/ link/ text/...
+		
+		//1- chỉ tương tác lên element này 1 lần (k khai báo biến)
+		driver.findElement(By.id("send2")).click();
+		
+		//2- element dùng lại nhiều lần trong trang hiện tại(khai báo biến)
+		WebElement loginButton = driver.findElement(By.id("send2"));
+		
+		loginButton.click();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
